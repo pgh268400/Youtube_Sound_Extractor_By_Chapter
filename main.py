68a971442a4ec430670bf337fcceda28b91e5329
File <pgh268400@naver.com>
2023-12-05 06:37:51 +0900

    UI 기능 단편 구현

--- main.py
+++ main.py
@@ -109,6 +109,9 @@ class InputWindow(QMainWindow, Ui_InputWindow):
         # 챕터 입력 상황 체크 변수
         self.need_chapter_input = False
 
+        # 이미 챕터가 존재하는 경우 사용할 것인지 체크하는 변수
+        self.use_already_chapter = None
+
         # 쓰레드 변수
         self.worker: Optional[DownloadWorker] = None
 
@@ -144,9 +147,11 @@ class InputWindow(QMainWindow, Ui_InputWindow):
         self.worker.update_log.connect(self.update_log)
         self.worker.chapter_enabled.connect(self.chapter_enabled)
         self.worker.failed_download.connect(self.failed_download)
+        self.worker.already_exist_chapter.connect(self.already_exist_chapter)
+
+        # 쓰레드 작업 시작
         self.worker.start()
 
-        # self.worker.wait()
         # print("작업 종료")
 
     # 해당 창을 종료하면 쓰레드 종료 & 전부 원상 복귀 시킨다.
@@ -183,12 +188,23 @@ class InputWindow(QMainWindow, Ui_InputWindow):
         self.btn_ok.setEnabled(True)
         self.backup_origin()
 
+    # 영상에 이미 챕터가 존재하는 경우
+    @Slot()
+    def already_exist_chapter(self) -> None:
+        self.use_already_chapter = QMessageBox.question(
+            self,
+            "알림",
+            "영상에 이미 챕터가 존재합니다 영상의 챕터를 사용하시겠습니까 ?",
+            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+        )
+
 
 class DownloadWorker(QThread):
     # Custom Signal 생성
     update_log = Signal(str)
     chapter_enabled = Signal()
     failed_download = Signal()
+    already_exist_chapter = Signal()
 
     def __init__(self, parent: InputWindow) -> None:
         # 부모 생성자 호출
@@ -196,6 +212,41 @@ class DownloadWorker(QThread):
 
         # 유저가 입력한 URL (Read Only)
         self.input_window = parent
+
+    # 유저로부터 챕터를 입력받고 파싱
+    def parse_chapter_by_user(self, duration) -> list[RangedChapter]:
+        self.update_log.emit("챕터를 위 박스에 입력해주세요. 다 입력했으면 C-OK 버튼을 누릅니다.")
+        self.chapter_enabled.emit()  # 챕터 박스 입력 명령을 메인 윈도우에 전달
+
+        # 챕터를 입력할 때까지 Worker 쓰레드는 대기 (메인 쓰레드의 동작 대기)
+        while not is_chapter_input_finish:
+            pass
+
+        # 챕터 입력이 완료되면 작업 시작
+        self.update_log.emit("챕터 생성 진행 중...")
+
+        input_chapters: str = self.input_window.textedit_user_input.toPlainText()
+        base_chapter = make_base_chapter(input_chapters)
+
+        pprint(base_chapter)
+
+        self.update_log.emit("챕터 변환을 수행합니다.")
+        ranged_chapter = convert_base_to_ranged_chapter(base_chapter, duration)
+        return ranged_chapter
+
+    # yt-dlp에서 가져온 챕터 내역을 프로그램에 알맞게 변환
+    def parse_chapter_by_yt_dlp(self, chapters: dict) -> list[RangedChapter]:
+        ranged_chapter = []
+        for chapter in chapters:
+            ranged_chapter.append(
+                RangedChapter(
+                    # filename_remover 작업
+                    title=filename_remover(chapter["title"]),
+                    start_time=chapter["start_time"],
+                    end_time=chapter["end_time"],
+                )
+            )
+        return ranged_chapter
 
     def run(self) -> None:
         try:
@@ -209,59 +260,31 @@ class DownloadWorker(QThread):
 
             title: str = info["title"]
             duration: str = info["duration_string"]
-            input_chapters = info["chapters"]
+            chapters = info["chapters"]
 
             self.update_log.emit(f"{title} | {duration}")
-            self.update_log.emit("챕터를 위 박스에 입력해주세요. 다 입력했으면 C-OK 버튼을 누릅니다.")
-            self.chapter_enabled.emit()  # 챕터 박스 입력 명령을 메인 윈도우에 전달
 
-            # 챕터를 입력할 때까지 Worker 쓰레드는 대기 (메인 쓰레드의 동작 대기)
-            while not is_chapter_input_finish:
-                pass
-
-            # 챕터 입력이 완료되면 작업 시작
-            self.update_log.emit("챕터 생성 진행 중...")
-
-            input_chapters: str = self.input_window.textedit_user_input.toPlainText()
-
-            # 유저로부터 챕터를 입력받고 파싱
-            def parse_chapter_by_user() -> list[RangedChapter]:
-                contents_join: str = input_chapters
-
-                self.update_log.emit("챕터 생성 진행 중...")
-                base_chapter = make_base_chapter(contents_join)
-                pprint(base_chapter)
-
-                self.update_log.emit("챕터 변환을 수행합니다.")
-                ranged_chapter = convert_base_to_ranged_chapter(base_chapter, duration)
-                return ranged_chapter
-
-            # yt-dlp에서 가져온 챕터 내역을 프로그램에 알맞게 변환
-            def parse_chapter_by_yt_dlp(chapters) -> list[RangedChapter]:
-                ranged_chapter = []
-                for i, chapter in enumerate(chapters):
-                    ranged_chapter.append(
-                        RangedChapter(
-                            # filename_remover 작업
-                            title=filename_remover(chapter["title"]),
-                            start_time=chapter["start_time"],
-                            end_time=chapter["end_time"],
-                        )
-                    )
-                return ranged_chapter
-
-            if input_chapters != None:
+            if chapters != None:
                 # 영상에 이미 챕터가 존재하는 경우 RangedChapter 에 맞게 변환한다.
-                answer = input("영상에 이미 챕터가 존재합니다 영상의 챕터를 사용하시겠습니까 ? (y/n) : ")
-                if answer.lower() == "y":
-                    input_chapters = parse_chapter_by_yt_dlp(input_chapters)
+                # 메인 쓰레드에게 메세지 입력을 요청하기 위해 시그널을 보내고 대기한다.
+                self.already_exist_chapter.emit()
+
+                # 메인 쓰레드가 사용자의 선택을 기다리는 동안 Worker 쓰레드는 대기한다.
+                while not self.input_window.use_already_chapter:
+                    pass
+
+                if (
+                    self.input_window.use_already_chapter
+                    == QMessageBox.StandardButton.Yes
+                ):
+                    chapters = self.parse_chapter_by_yt_dlp(chapters)
                 else:
-                    input_chapters = parse_chapter_by_user()
+                    chapters = self.parse_chapter_by_user(duration)
             else:
-                input_chapters = parse_chapter_by_user()
+                chapters = self.parse_chapter_by_user(duration)
 
             # 여기까지 오면 chapters는 확정된 상태 = Not Empty
-            pprint(input_chapters)
+            pprint(chapters)
 
             # filename_remover 작업
             title = filename_remover(title)
@@ -274,7 +297,7 @@ class DownloadWorker(QThread):
 
             ext = "mp4"  # 수정 금지!
 
-            print("최고 품질로 영상을 다운로드 합니다.")
+            self.update_log.emit("최고 품질로 영상을 다운로드 합니다.")
 
             with yt_dlp.YoutubeDL(
                 {
@@ -287,16 +310,22 @@ class DownloadWorker(QThread):
                 }
             ) as ydl:
                 ydl.download([url])
-            print("다운로드가 완료되었습니다.")
+            self.update_log.emit("다운로드가 완료되었습니다.")
 
             # 각 챕터 시작 시간의 썸네일을 ffmpeg를 이용해 추출한다.
-            print("썸네일을 추출합니다.")
+            self.update_log.emit("썸네일을 추출합니다.")
+
+            # 설정 파일에서 설정값을 가져옴
+            thumbnail_folder = config.get().thumbnail_folder
+            output_folder = config.get().output_folder
+            custom_offset = config.get().custom_offset
+            total_offset = config.get().total_offset
 
             # 썸네일 저장용 폴더 생성
             os.makedirs(thumbnail_folder, exist_ok=True)
 
             # ffmpeg로 챕터 시작 시간의 썸네일 추출
-            for i, chapter in enumerate(input_chapters):
+            for i, chapter in enumerate(chapters):
                 copy_offset = total_offset
                 # custom offset이 적용된 경우 해당 순번의 썸네일에만 오프셋을 따로 줌 (total_offset과 합산됨)
                 if i + 1 in custom_offset:
@@ -317,18 +346,18 @@ class DownloadWorker(QThread):
                     ]
                 )
                 print(output)
-            print("썸네일 추출이 완료되었습니다.")
+            self.update_log.emit("썸네일 추출이 완료되었습니다.")
 
             # 다운로드 받은 영상에서 무손실 음원 m4a를 추출한다
-            print("음원을 추출합니다.")
+            self.update_log.emit("음원을 추출합니다.")
             output = run_ffmpeg(
                 ["-y", "-i", f"{title}.{ext}", "-vn", "-acodec", "copy", f"{title}.m4a"]
             )
 
             # 추출한 음원을 챕터별로 자른다.
-            print("음원을 챕터별로 자릅니다.")
+            self.update_log.emit("음원을 챕터별로 자릅니다.")
             os.makedirs(output_folder, exist_ok=True)
-            for i, chapter in enumerate(input_chapters):
+            for i, chapter in enumerate(chapters):
                 # 챕터별 음원 추출
                 output = run_ffmpeg(
                     [
@@ -344,26 +373,26 @@ class DownloadWorker(QThread):
                         os.path.join(output_folder, f"{chapter.title}.m4a"),
                     ]
                 )
-                print(f"{chapter.title}.m4a 추출 완료")
+                self.update_log.emit(f"{chapter.title}.m4a 추출 완료")
             # 챕터별로 자른 음원에 썸네일을 붙인다.
-            print("썸네일을 적용합니다.")
+            self.update_log.emit("썸네일을 적용합니다.")
 
             # 썸네일 폴더의 모든 파일을 가져온다.
             # thumbnail_files = os.listdir(thumbnail_folder)
             thumbnail_files = glob.glob(os.path.join(thumbnail_folder, "*.png"))
             thumbnail_files = natsorted(thumbnail_files)
 
-            for i, chapter in enumerate(input_chapters):
+            for i, chapter in enumerate(chapters):
                 m4a_path = os.path.join(output_folder, chapter.title + ".m4a")
                 add_album_art(m4a_path, thumbnail_files[i])
-                print(f"{chapter.title}.m4a 썸네일 적용 완료")
+                self.update_log.emit(f"{chapter.title}.m4a 썸네일 적용 완료")
 
             # 작업 완료 후 다운로드 받은 영상, 추출한 음원, 썸네일 폴더를 삭제한다.
             # os.remove(f"{title}.{ext}")
             # os.remove(f"{title}.m4a")
             # shutil.rmtree(thumbnail_folder)
 
-            print("작업이 완료되었습니다.")
+            self.update_log.emit("작업이 완료되었습니다.")
 
         except Exception as e:
             self.update_log.emit("유튜브 영상 정보를 가져오는데 실패했습니다.")
